76b8203e65d2409ef2641f1bd7060c645dd269c4
Jony-007 <habiburrahmanjony4@gmail.com>
2021-11-20 16:03:21 +0600

    learned how to use template strings

---
+++ operator_precedence/o_p.js
@@ -0,0 +1,10 @@
+const firstName = 'jony';
+const job = 'teacher';
+const birthYear = 1996;
+let currentYear = 2021;
+
+// use backtick to use operator precedence
+
+const jony = `I'm ${firstName}, a ${currentYear - birthYear} years old teacher`;
+
+console.log(jony);
