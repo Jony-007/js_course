7bd5d0de1cc6bfcac59480e1594ef0c8cf212287
Jony-007 <habiburrahmanjony4@gmail.com>
2021-11-20 16:08:01 +0600

    learn to create new lines with template strings

--- operator_precedence/o_p.js
+++ operator_precedence/o_p.js
@@ -8,3 +8,11 @@ let currentYear = 2021;
 const jony = `I'm ${firstName}, a ${currentYear - birthYear} years old teacher`;
 
 console.log(jony);
+
+//  how to create new lines with template strings
+
+// use template strings and then use enter
+
+console.log(` I am
+jony`);
+
